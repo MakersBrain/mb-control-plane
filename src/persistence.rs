e183dc0af5983536251e278d7de1bf3caf307dbb
Caddy Glow <219252898+CaddyGlow@users.noreply.github.com>
2026-08-24 09:09:47 +0200

    fix: embed schema epoch 0041

--- src/persistence.rs
+++ src/persistence.rs
@@ -8,7 +8,7 @@ use uuid::Uuid;
 
 use crate::domain::{IntegrationError, OperationKind};
 
-pub const EMBEDDED_SCHEMA_RELEASE: &str = "0038_release_generation_retention";
+pub const EMBEDDED_SCHEMA_RELEASE: &str = "0041_interrupted_release_runtime_receipt_review";
 
 #[derive(Clone)]
 pub struct Store {
